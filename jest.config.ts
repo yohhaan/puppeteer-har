7b8531398c2c39e2a6bf4c2253b2c9641a175146
Philipp Giese <philipp.giese@auteon.de>
2021-05-11 09:30:53 +0200

    chore: only run specs in src dir

--- jest.config.ts
+++ jest.config.ts
@@ -145,11 +145,7 @@ export default {
   // Adds a location field to test results
   // testLocationInResults: false,
 
-  // The glob patterns Jest uses to detect test files
-  // testMatch: [
-  //   "**/__tests__/**/*.[jt]s?(x)",
-  //   "**/?(*.)+(spec|test).[tj]s?(x)"
-  // ],
+  testMatch: ["<rootDir>/src/**/*.spec.ts"],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
